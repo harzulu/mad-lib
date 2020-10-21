ea3fd3f29af66e0acb7cb7e108cf386ec6778163
Jerrod Styrk <jstyrk@citadel.edu>
2020-10-21 11:17:59 -0700

    fill out the javascript and apply function
    Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
-$(document).ready(function() {
+// Business Logic
 
+
+// User Interface Logic
+$(document).ready(function() {
+  $("#formOne").submit(function(event){
+    let nameInput = $("input#name").val();
+    let celebrityInput = $("input#celebrity").val();
+    let verbInput = $("input#verb").val();
+    let insectInput = $("input#insects").val();
+    let exclameInput = $("input#exclamation").val();
+    let placeInput = $("input#place").val();
+    let nounInput = $("input#noun").val();
+
+    $(".yourName").append(nameInput);
+    $(".celebrity").append(celebrityInput);
+    $(".verb").append(verbInput);
+    $(".insects").append(insectInput);
+    $(".exclamation").append(exclameInput);
+    $(".place").append(placeInput);
+    $(".noun").append(nounInput);
+
+    $("#story").show();
+    $(".fields").hide();
+
+    $("#restart").click(function(){
+      window.location.reload();
+    });
+    event.preventDefault();
+  });
 });
